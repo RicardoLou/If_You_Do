007853de543dfc2e1b0aa2245e088062ef524845
繁叶秋水 <1636650169@qq.com>
2024-12-19 19:35:09 +0800

    test

---
+++ 段传相/test/MyClass.h
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+class MyClass
+{
+public:
+	MyClass();
+	~MyClass();
+	void print();
+
+private:
+	
+};
+
+MyClass::MyClass()
+{
+}
+
+MyClass::~MyClass()
+{
+}
