da7d48046e280fa491a1105802c1d8814fa07e21
RicardoLou <Anglyao778@gmail.com>
2024-12-19 23:58:53 +0800

    LeetCodeDayly

--- 娄子熇/LeetCodeDayly/lc3285.cpp
+++ 娄子熇/LeetCodeDayly/lc3285.cpp
@@ -4,7 +4,12 @@ using namespace std;
 class Solution {
 public:
     vector<int> stableMountains(vector<int>& height, int threshold) {
-        // todo
-        int n = height.size();
+        vector<int> ans;
+        for (int i = 0; i + 1 < height.size(); i++) {
+            if (height[i] > threshold) {
+                ans.push_back(i + 1);
+            }
+        }
+        return ans;
     }
 };
