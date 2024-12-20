24eb33d72aad33616ed8bd7ff0d2e90dcd82c91c
yushengsanyue <1846134706@qq.com>
2024-12-20 15:06:54 +0800

    yushengsanyue first questioncd

--- 余家齐/L1-002.cpp
+++ 余家齐/L1-002.cpp
@@ -2,13 +2,24 @@
 using namespace std;
 int main()
 {
-    int n,i,j,s;
+    int n,i,j,s=1,x;
     cin >> n;
-    for(i=3,j=1,s=1;s<=n;i+=2)
-    {
+    for(i=1,j=1;s<=n;i+=2){
         s+=i*2;
         j++;
     }
-    cout << j-1 << endl;
-    cout << n-(s-(i*2)) << endl;
+    j-=1;
+    cout << j << endl;
+    x=n-(s-(i*2));
+    for(i=j,x=0;i>=0;i--){
+        for(int k=0;k<=x-1;k++){
+            cout << " ";
+        }
+        x++;
+        for(int k=1;k<=i*2-1;k++){
+            cout << "*";
+        }
+        cout << endl;
+    }
+    cout << x;
 }
