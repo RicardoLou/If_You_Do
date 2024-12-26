b0efb1c67bc7f40bf4e4f4bf5db198573733408d
Yours-HanKing <2300176664@qq.com>
2024-12-26 23:17:13 +0800

    testSSL

--- 陈新聪/test.cpp
+++ 陈新聪/test.cpp
@@ -10,5 +10,32 @@ int main() {
     }
     int ans = 0;
     // c
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(-1), *p = &dummy;
+        ListNode *p1 = l1, *p2 = l2;
 
+        while (p1 != nullptr && p2 != nullptr) {
+            if (p1->val > p2->val) {
+                p->next = p2;
+                p2 = p2->next;
+            } else {
+                p->next = p1;
+                p1 = p1->next;
+            }
+            p = p->next;
+        }
+
+        if (p1 != nullptr) {
+            p->next = p1;
+        }
+
+        if (p2 != nullptr) {
+            p->next = p2;
+        }
+
+        return dummy.next;
+    }
+};
 }
